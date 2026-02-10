5c3a96ed3fe59a43fc9f07186b6a5561e43389ac
OskarEriksen <omle.eriksen@gmail.com>
2026-02-10 14:35:15 +0100

    feat: add prisma adapter to auth.js file

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,13 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./prisma";
 
+const adapter = PrismaAdapter(prisma);
+
 export const { auth, handlers, signIn } = NextAuth({
+  adapter,
   providers: [
     GitHub,
     Credentials({
