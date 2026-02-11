a5e69eb823f59a4ffe3862b85951c2e85dfacff5
OskarEriksen <omle.eriksen@gmail.com>
2026-02-11 18:43:41 +0100

    feat: if user doesn't belong to a organization, redirect to /dashboard/organization

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,21 @@
 import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/db/prisma";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
   const session = await auth();
   if (!session) redirect("/sign-in");
 
-  console.log("Session:", session);
+  const memberships = await prisma.userOrganization.findMany({
+    where: { userId: session?.user?.id },
+    include: { organization: true },
+  });
 
-  return <div>Dashboard</div>;
+  if (memberships.length === 0) redirect("/dashboard/organizations");
+
+  console.log("User Organizations:", memberships);
+
+  return <div className="flex h-full w-full bg-amber-800 rounded-b-lg">Dashboard</div>;
 };
 
 export default Page;
