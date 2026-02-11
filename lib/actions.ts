31039186bb9881f179b2f1e30cc00dec2896c6fc
OskarEriksen <omle.eriksen@gmail.com>
2026-02-11 16:48:06 +0100

    feat: create server action to create organization

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,13 +1,14 @@
+import { auth } from "./auth";
 import { prisma } from "./db/prisma";
-import { schema } from "./db/schema";
+import { organizationSchema, userSchema } from "./db/schema";
 import { executeAction } from "./executeAction";
 
-const signUp = async (formData: FormData) => {
+export const signUp = async (formData: FormData) => {
   return executeAction({
     actionFn: async () => {
       const email = formData.get("email");
       const password = formData.get("password");
-      const validatedData = schema.parse({ email, password });
+      const validatedData = userSchema.parse({ email, password });
       await prisma.user.create({
         data: {
           email: validatedData.email,
@@ -18,4 +19,44 @@ const signUp = async (formData: FormData) => {
   });
 };
 
-export { signUp };
+export const createOrganization = async (formData: FormData) => {
+  return executeAction({
+    actionFn: async () => {
+      const session = await auth();
+      if (!session?.user?.id) {
+        throw new Error("Unauthorized");
+      }
+
+      const name = formData.get("name");
+      const street = formData.get("street");
+      const postalCode = formData.get("postalCode");
+      const city = formData.get("city");
+      const cvrNumber = formData.get("cvrNumber");
+      const logo = formData.get("logo");
+      const validatedData = organizationSchema.parse({
+        name,
+        street,
+        postalCode,
+        city,
+        cvrNumber,
+        logo,
+      });
+      await prisma.organization.create({
+        data: {
+          name: validatedData.name,
+          street: validatedData.street,
+          postalCode: validatedData.postalCode,
+          city: validatedData.city,
+          cvrNumber: validatedData.cvrNumber,
+          logo: validatedData.logo,
+          userOrganizations: {
+            create: {
+              userId: session.user.id,
+              isAdmin: true,
+            },
+          },
+        },
+      });
+    },
+  });
+};
