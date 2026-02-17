a41010b7be081a033db728e0c99f0ce737c1fb00
OskarEriksen <omle.eriksen@gmail.com>
2026-02-17 16:52:34 +0100

    feat: add initial Page component for info screen

---
+++ src/app/dashboard/info-screen/see-all/page.tsx
@@ -0,0 +1,12 @@
+const Page = () => {
+  return (
+    <div
+      className="flex flex-col min-h-0 w-full gap-4 rounded-lg p-4"
+      suppressHydrationWarning
+    >
+      Page
+    </div>
+  );
+};
+
+export default Page;
