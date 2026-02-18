455d158383640cca49f635455791e41061df158c
OskarEriksen <omle.eriksen@gmail.com>
2026-02-18 10:35:21 +0100

    feat: fetch all info-screens and colours for each screen

--- src/app/dashboard/info-screen/see-all/page.tsx
+++ src/app/dashboard/info-screen/see-all/page.tsx
@@ -1,4 +1,21 @@
-const Page = () => {
+import { fetchColour } from "@/lib/colourAction";
+import { fetchAllInfoScreenForOrganization } from "@/lib/infoScreenActions";
+import { cookies } from "next/headers";
+
+const Page = async () => {
+    const cookieStore = await cookies();
+    const selectedOrganizationId = cookieStore.get(
+      "selectedOrganizationId",
+    )?.value;
+
+  const infoScreens = await fetchAllInfoScreenForOrganization(selectedOrganizationId ?? "");
+
+  infoScreens.data?.forEach(async (infoScreen) => {
+    const colour = await fetchColour(infoScreen.colourId);
+    console.log(colour);
+  })
+
+  console.log(infoScreens);
   return (
     <div
       className="flex flex-col min-h-0 w-full gap-4 rounded-lg p-4"
