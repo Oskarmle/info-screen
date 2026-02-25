7fb942f992607e3a71e4126205deb5c2a12a4b77
OskarEriksen <omle.eriksen@gmail.com>
2026-02-25 16:39:06 +0100

    feat: implement Droppable component for drag-and-drop functionality

---
+++ src/components/dragdrop/Droppable.tsx
@@ -0,0 +1,26 @@
+import { useDroppable } from "@dnd-kit/react";
+import { ReactNode } from "react";
+
+interface DroppableProps {
+  id: string;
+  children?: ReactNode;
+  listName: string;
+}
+
+const Droppable = ({ id, children, listName }: DroppableProps) => {
+  const { ref } = useDroppable({
+    id,
+    type: "column",
+    accept: ["item"],
+  });
+  return (
+    <div ref={ref} className="h-60 bg-accent rounded-lg flex flex-col p-2">
+      <h1 className="text-lg font-bold pb-2 shrink-0">{listName}</h1>
+      <div className="flex flex-row gap-2 flex-1 min-h-0 overflow-auto">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Droppable;
