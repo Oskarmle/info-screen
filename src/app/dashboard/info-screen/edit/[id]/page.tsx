7a21bbc453d19f477afc655a4f26aa88ebf39e07
OskarEriksen <omle.eriksen@gmail.com>
2026-02-25 16:39:32 +0100

    feat: enhance edit page with content fetching and drag-and-drop functionality

--- src/app/dashboard/info-screen/edit/[id]/page.tsx
+++ src/app/dashboard/info-screen/edit/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { fetchAllContentForOrganization } from "@/lib/contentActions";
 import { fetchInfoScreen } from "@/lib/infoScreenActions";
+import ContentDragDrop from "@/src/components/dragdrop/ContentDragDrop";
+import { Separator } from "@/src/components/ui/separator";
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
@@ -6,9 +9,28 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   console.log("Fetched info screen in page:", infoScreen);
 
+  const contents = await fetchAllContentForOrganization(
+    infoScreen.data?.organizationId || "",
+  );
+
+  console.log("Fetched contents in page:", contents);
+
   return (
-    <div>
-      <h1>{infoScreen.data?.title}</h1>
+    <div
+      className="flex flex-col min-h-0 h-full w-full gap-4 rounded-lg p-4"
+      suppressHydrationWarning
+    >
+      <h1 className="text-2xl font-bold">Edit your info screen</h1>
+      <p>
+        Here you can change the title, description, color and more. You can also
+        add content to your info screen
+      </p>
+      <Separator />
+      <div className="flex flex-col gap-1">
+        <h1 className="text-xl">{infoScreen.data?.title}</h1>
+        <p>{infoScreen.data?.description}</p>
+      </div>
+      <ContentDragDrop contents={contents?.data || []} />
     </div>
   );
 };
