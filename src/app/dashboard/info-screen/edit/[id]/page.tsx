86d49dccaf4426c852a24fd32b112effd6c499aa
OskarEriksen <omle.eriksen@gmail.com>
2026-02-26 16:31:02 +0100

    feat: enhance edit info screen by adding error handling and refactoring layout

--- src/app/dashboard/info-screen/edit/[id]/page.tsx
+++ src/app/dashboard/info-screen/edit/[id]/page.tsx
@@ -1,11 +1,14 @@
 import { fetchAllContentForOrganization } from "@/lib/contentActions";
 import { fetchInfoScreen } from "@/lib/infoScreenActions";
 import ContentDragDrop from "@/src/components/dragdrop/ContentDragDrop";
+import EditInfoScreen from "@/src/components/EditInfoScreen";
 import { Separator } from "@/src/components/ui/separator";
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   const infoScreen = await fetchInfoScreen(id);
+
+  if (!infoScreen.data) return <div>Info screen not found</div>;
 
   const contents = await fetchAllContentForOrganization(
     infoScreen.data?.organizationId || "",
@@ -17,19 +20,12 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
       suppressHydrationWarning
     >
       <h1 className="text-2xl font-bold">Edit your info screen</h1>
-      <p>
-        Here you can change the title, description, color and more. You can also
-        add content to your info screen
-      </p>
-      <Separator />
-      <div className="flex flex-col gap-1">
-        <h1 className="text-xl">{infoScreen.data?.title}</h1>
-        <p>{infoScreen.data?.description}</p>
-      </div>
       <ContentDragDrop
         infoScreenId={infoScreen.data?.id || ""}
         contents={contents?.data || []}
       />
+      <Separator />
+      <EditInfoScreen infoScreen={infoScreen?.data} />
     </div>
   );
 };
