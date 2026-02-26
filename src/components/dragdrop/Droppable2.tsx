c9a28c8ea65e292e1ae0ecacad2aa8ce01a66aad
OskarEriksen <omle.eriksen@gmail.com>
2026-02-26 15:42:37 +0100

    feat: add row-styled droppable

--- src/components/dragdrop/Droppable2.tsx
+++ src/components/dragdrop/Droppable2.tsx
@@ -7,20 +7,23 @@ interface DroppableProps {
   listName: string;
 }
 
-const Droppable = ({ id, children, listName }: DroppableProps) => {
+const Droppable2 = ({ id, children, listName }: DroppableProps) => {
   const { ref } = useDroppable({
     id,
     type: "column",
     accept: ["item"],
   });
   return (
-    <div ref={ref} className="bg-accent rounded-lg flex flex-col w-full p-2">
+    <div
+      ref={ref}
+      className="h-80 w-1/3 bg-accent rounded-lg flex flex-col gap-2 p-2"
+    >
       <h1 className="text-lg font-bold pb-2 shrink-0">{listName}</h1>
-      <div className="flex flex-row gap-2 flex-1 min-h-0 overflow-auto rounded-lg">
+      <div className="flex flex-col w-full gap-2 overflow-auto rounded-lg">
         {children}
       </div>
     </div>
   );
 };
 
-export default Droppable;
+export default Droppable2;
