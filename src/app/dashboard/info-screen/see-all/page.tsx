3ecd4f5c9531f1e0e9a28d327fb73fdef0ea06fa
OskarEriksen <omle.eriksen@gmail.com>
2026-03-04 12:38:03 +0100

    fix: adjust padding in info screens layout for better spacing

--- src/app/dashboard/info-screen/see-all/page.tsx
+++ src/app/dashboard/info-screen/see-all/page.tsx
@@ -25,7 +25,7 @@ const Page = async () => {
 
   return (
     <div
-      className="flex flex-col min-h-0 h-full w-full gap-4 rounded-lg p-4"
+      className="flex flex-col min-h-0 h-full w-full gap-4 rounded-lg px-4 pb-4"
       suppressHydrationWarning
     >
       <h1 className="text-2xl font-bold">This is all the info screens</h1>
