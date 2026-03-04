d1b8b77f5a423916a0eaa8623de4dd266fd26afa
OskarEriksen <omle.eriksen@gmail.com>
2026-03-04 12:38:18 +0100

    feat: initial see all page for content

--- src/app/dashboard/content/see-all/page.tsx
+++ src/app/dashboard/content/see-all/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { fetchAllContentForOrganization } from "@/lib/contentActions";
+import { ScrollArea } from "@/src/components/ui/scroll-area";
+import { cookies } from "next/headers";
 
-const Page = () => {
+const Page = async () => {
+  const cookieStore = await cookies();
+  const selectedOrganizationId = cookieStore.get(
+    "selectedOrganizationId",
+  )?.value;
+
+  const content = (
+    await fetchAllContentForOrganization(selectedOrganizationId ?? "")
+  ).data;
+
+  console.log(content);
+
   return (
-    <div>See all content</div>
-  )
-}
+    <div
+      className="flex flex-col min-h-0 h-full w-full gap-4 rounded-lg px-4 pb-4"
+      suppressHydrationWarning
+    >
+      <h1 className="text-2xl font-bold">This is all the info screens</h1>
+      <p>
+        This list shows all the content that have been created for this
+        organization. You can&apos;t delete that is active on an info screen,
+        but you can delete content that is not active on any info screen.
+      </p>
+      <ScrollArea className="flex-1 flex-col min-h-0 gap-4 pr-4 rounded-lg">
+        <div className="flex flex-row flex-wrap pb-4 justify-between gap-4">
+          <div className="w-[32%] h-100 bg-amber-100 rounded-lg"></div>
+          <div className="w-[32%] h-100 bg-amber-100 rounded-lg"></div>
+          <div className="w-[32%] h-100 bg-amber-100 rounded-lg"></div>
+          <div className="w-[32%] h-100 bg-amber-100 rounded-lg"></div>
+          <div className="w-[32%] h-100 bg-amber-100 rounded-lg"></div>
+          <div className="w-[32%] h-100 bg-amber-100 rounded-lg"></div>
+          <div className="w-[32%] h-100 bg-amber-100 rounded-lg"></div>
+          <div className="w-[32%] h-100 bg-amber-100 rounded-lg"></div>
+          <div className="w-[32%] h-100 bg-amber-100 rounded-lg"></div>
+        </div>
+      </ScrollArea>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
