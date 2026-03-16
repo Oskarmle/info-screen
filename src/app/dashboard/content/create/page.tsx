10b341fd851af80cd84d8d135d9b39b4f1ed2b67
OskarEriksen <omle.eriksen@gmail.com>
2026-03-16 12:39:52 +0100

    fix: add scrollable area to creating content page

--- src/app/dashboard/content/create/page.tsx
+++ src/app/dashboard/content/create/page.tsx
@@ -10,6 +10,7 @@ import {
   FieldSet,
 } from "@/src/components/ui/field";
 import { Input } from "@/src/components/ui/input";
+import { ScrollArea } from "@/src/components/ui/scroll-area";
 import { Textarea } from "@/src/components/ui/textarea";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
@@ -25,7 +26,7 @@ const Page = async () => {
 
   return (
     <div
-      className="flex flex-col min-h-0 w-full gap-4 rounded-lg p-4"
+      className="flex min-h-0 flex-1 w-full flex-col gap-4 overflow-hidden rounded-lg p-4"
       suppressHydrationWarning
     >
       <div className="flex flex-col gap-1">
@@ -36,139 +37,143 @@ const Page = async () => {
           edit/delete thh content later.
         </p>
       </div>
-      <div className="flex items-center flex-col">
-        <form
-          className="flex mt-4 min-h-0 w-full"
-          action={async (formData: FormData) => {
-            "use server";
-            const res = await createContent(formData);
-            if (res.success) {
-              redirect("/dashboard/content/see-all");
-            }
-          }}
-        >
-          <input
-            type="hidden"
-            name="organizationId"
-            value={selectedOrganizationId ?? ""}
-          />
-          <FieldGroup>
-            <FieldSet>
-              <FieldGroup>
-                <Field>
-                  <FieldLabel htmlFor="name">
-                    Give a name to your content
-                  </FieldLabel>
-                  <Input
-                    id="name"
-                    placeholder="Name"
-                    required
-                    name="name"
-                    type="text"
-                    autoComplete="name"
-                  />
-                  <FieldDescription>
-                    This will help you find it later when you want to add it to
-                    an info screen. It will not be used as the title for your
-                    info screen.
-                  </FieldDescription>
-                </Field>
-
-                <FieldSeparator />
-
-                <Field>
-                  <FieldLabel htmlFor="title">
-                    Give your content a title
-                  </FieldLabel>
-                  <Input
-                    id="title"
-                    placeholder="Title"
-                    required
-                    name="title"
-                    type="text"
-                    autoComplete="title"
-                  />
-                  <FieldDescription>
-                    This will be shown on the info-screen for this piece of
-                    content.
-                  </FieldDescription>
-                </Field>
-
-                <Field>
-                  <FieldLabel htmlFor="text">
-                    The main text for your content
-                  </FieldLabel>
-                  <Textarea
-                    placeholder="Write the main text for your content here"
-                    id="text"
-                    required
-                    name="text"
-                    autoComplete="text"
-                    className="h-40"
-                  />
-                </Field>
-
-                <Field>
-                  <FieldLabel htmlFor="image">
-                    Add an image (optional)
-                  </FieldLabel>
-                  <Input type="file" id="image" name="image" />
-                  <FieldDescription>
-                    Select a image to upload. This is optional.
-                  </FieldDescription>
-                </Field>
-              </FieldGroup>
-            </FieldSet>
-            <FieldSet>
-              <FieldGroup>
-                <FieldLabel htmlFor="contactName" className="text-lg">
-                  Add contact information (optional)
-                </FieldLabel>
-                <div className="flex flex-row w-full gap-4">
+      <ScrollArea className="min-h-0 flex-1 pr-4">
+        <div className="w-full">
+          <form
+            className="mt-4 flex min-h-0 w-full"
+            action={async (formData: FormData) => {
+              "use server";
+              const res = await createContent(formData);
+              if (res.success) {
+                redirect("/dashboard/content/see-all");
+              }
+            }}
+          >
+            <input
+              type="hidden"
+              name="organizationId"
+              value={selectedOrganizationId ?? ""}
+            />
+            <FieldGroup>
+              <FieldSet>
+                <FieldGroup>
                   <Field>
-                    <FieldLabel htmlFor="contactEmail">
-                      Contact email
+                    <FieldLabel htmlFor="name">
+                      Give a name to your content
                     </FieldLabel>
                     <Input
-                      id="contactEmail"
-                      placeholder="E-mail"
-                      name="contactEmail"
+                      id="name"
+                      placeholder="Name"
+                      required
+                      name="name"
                       type="text"
-                      autoComplete="contactEmail"
+                      autoComplete="name"
+                    />
+                    <FieldDescription>
+                      This will help you find it later when you want to add it
+                      to an info screen. It will not be used as the title for
+                      your info screen.
+                    </FieldDescription>
+                  </Field>
+
+                  <FieldSeparator />
+
+                  <Field>
+                    <FieldLabel htmlFor="title">
+                      Give your content a title
+                    </FieldLabel>
+                    <Input
+                      id="title"
+                      placeholder="Title"
+                      required
+                      name="title"
+                      type="text"
+                      autoComplete="title"
+                    />
+                    <FieldDescription>
+                      This will be shown on the info-screen for this piece of
+                      content.
+                    </FieldDescription>
+                  </Field>
+
+                  <Field>
+                    <FieldLabel htmlFor="text">
+                      The main text for your content
+                    </FieldLabel>
+                    <Textarea
+                      placeholder="Write the main text for your content here"
+                      id="text"
+                      required
+                      name="text"
+                      autoComplete="text"
+                      className="h-40"
                     />
                   </Field>
 
                   <Field>
-                    <FieldLabel htmlFor="contactName">Contact name</FieldLabel>
-                    <Input
-                      id="contactName"
-                      placeholder="First and last name"
-                      name="contactName"
-                      type="text"
-                      autoComplete="contactName"
-                    />
+                    <FieldLabel htmlFor="image">
+                      Add an image (optional)
+                    </FieldLabel>
+                    <Input type="file" id="image" name="image" />
+                    <FieldDescription>
+                      Select a image to upload. This is optional.
+                    </FieldDescription>
                   </Field>
-                </div>
-                <FieldDescription>
-                  If you want to add contact information for this content, you
-                  can add it here. This is optional. It will be shown as
-                  <span>
-                    &quot;For more information, contact FirstName LastName on
-                    email@email.com&quot;
-                  </span>
-                  on the info-screen.
-                </FieldDescription>
-              </FieldGroup>
-            </FieldSet>
-            <Field>
-              <Button type="submit">Create content</Button>
-              <Button variant="outline" type="button">
-                Cancel
-              </Button>
-            </Field>
-          </FieldGroup>
-        </form>
-      </div>
+                </FieldGroup>
+              </FieldSet>
+              <FieldSet>
+                <FieldGroup>
+                  <FieldLabel htmlFor="contactName" className="text-lg">
+                    Add contact information (optional)
+                  </FieldLabel>
+                  <div className="flex flex-row w-full gap-4">
+                    <Field>
+                      <FieldLabel htmlFor="contactEmail">
+                        Contact email
+                      </FieldLabel>
+                      <Input
+                        id="contactEmail"
+                        placeholder="E-mail"
+                        name="contactEmail"
+                        type="text"
+                        autoComplete="contactEmail"
+                      />
+                    </Field>
+
+                    <Field>
+                      <FieldLabel htmlFor="contactName">
+                        Contact name
+                      </FieldLabel>
+                      <Input
+                        id="contactName"
+                        placeholder="First and last name"
+                        name="contactName"
+                        type="text"
+                        autoComplete="contactName"
+                      />
+                    </Field>
+                  </div>
+                  <FieldDescription>
+                    If you want to add contact information for this content, you
+                    can add it here. This is optional. It will be shown as
+                    <span>
+                      &quot;For more information, contact FirstName LastName on
+                      email@email.com&quot;
+                    </span>
+                    on the info-screen.
+                  </FieldDescription>
+                </FieldGroup>
+              </FieldSet>
+              <Field>
+                <Button type="submit">Create content</Button>
+                <Button variant="outline" type="button">
+                  Cancel
+                </Button>
+              </Field>
+            </FieldGroup>
+          </form>
+        </div>
+      </ScrollArea>
     </div>
   );
 };
