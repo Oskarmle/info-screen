74f005a93ea0c36abcfded34bd6f5aac1f76522d
OskarEriksen <omle.eriksen@gmail.com>
2026-03-19 16:27:47 +0100

    feat: make sponsor banner a separate component

---
+++ src/components/infoscreen/SponsorBanner.tsx
@@ -0,0 +1,9 @@
+const SponsorBanner = () => {
+  return (
+    <div className="w-full bg-accent border-t h-32 flex items-center justify-center">
+      <h1>Sponsor banner</h1>
+    </div>
+  );
+};
+
+export default SponsorBanner;
