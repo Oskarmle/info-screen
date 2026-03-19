247cdfc89e1f078a752f2d427902cf53a1b23112
OskarEriksen <omle.eriksen@gmail.com>
2026-03-19 15:39:44 +0100

    feat: add custom colour from db to the info screen

--- src/app/info-screen/[id]/page.tsx
+++ src/app/info-screen/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { fetchColour } from "@/lib/colourAction";
 import { fetchInfoScreen } from "@/lib/infoScreenActions";
 import InfoscreenCarousel from "@/src/components/infoscreen/InfoscreenCarousel";
 
@@ -5,12 +6,17 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   const infoScreen = await fetchInfoScreen(id);
 
+  const colour = await fetchColour(infoScreen.data?.colourId || "");
+
   return (
     <div className="w-screen h-screen flex flex-col">
-      <div className="h-full w-screen">
-        <InfoscreenCarousel infoscreenId={infoScreen.data?.id || ""} />
+      <div className="h-full w-screen p-4 shadow-2xl">
+        <InfoscreenCarousel
+          infoscreenId={infoScreen.data?.id || ""}
+          colour={colour.data?.oklch || "#ffffff"}
+        />
       </div>
-      <div className="w-full bg-amber-700 h-32 flex items-center justify-center">
+      <div className="w-full bg-accent border-t h-32 flex items-center justify-center">
         <h1>Sponsor banner</h1>
       </div>
     </div>
--- src/components/infoscreen/InfoscreenCarousel.tsx
+++ src/components/infoscreen/InfoscreenCarousel.tsx
@@ -3,15 +3,20 @@ import InfoscreenCarouselClient from "./InfoscreenCarouselClient";
 
 type InfoscreenCarouselProps = {
   infoscreenId: string;
+  colour: string;
 };
 
 const InfoscreenCarousel = async ({
   infoscreenId,
+  colour,
 }: InfoscreenCarouselProps) => {
   const content = await fetchAllContentForInfoScreen(infoscreenId);
 
   return (
-    <InfoscreenCarouselClient contentData={content.data || []} />
+    <InfoscreenCarouselClient
+      contentData={content.data || []}
+      colour={colour}
+    />
   );
 };
 
