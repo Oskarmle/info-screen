51ef3e2c885c5e87361d72a3bdc4b8065bfaf34f
OskarEriksen <omle.eriksen@gmail.com>
2026-03-23 12:02:31 +0100

    feat: add sponsors to info screens

--- src/components/infoscreen/SponsorBanner.tsx
+++ src/components/infoscreen/SponsorBanner.tsx
@@ -1,7 +1,34 @@
-const SponsorBanner = () => {
+import { fetchAllSponsorsForOrganization } from "@/lib/sponsorActions";
+import { cookies } from "next/headers";
+import Image from "next/image";
+import { Separator } from "../ui/separator";
+
+const SponsorBanner = async () => {
+  const cookieStore = await cookies();
+  const selectedOrganizationId = cookieStore.get(
+    "selectedOrganizationId",
+  )?.value;
+
+  const sponsors = await fetchAllSponsorsForOrganization(
+    selectedOrganizationId ?? "",
+  );
+
+  console.log("Sponsors for organization:", sponsors.data);
+
   return (
-    <div className="w-full bg-accent border-t h-32 flex items-center justify-center">
-      <h1>Sponsor banner</h1>
+    <div className="flex flex-row gap-8 px-4 w-full bg-accent border-t h-32 items-center">
+      {sponsors.data?.map((sponsor) => (
+        <div key={sponsor.id}>
+          <div className="relative h-20 w-48 rounded-lg">
+            <Image
+              src={sponsor.logo || "/placeholder-logo.png"}
+              fill
+              className="rounded-lg object-contain"
+              alt="Sponsor logo"
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
